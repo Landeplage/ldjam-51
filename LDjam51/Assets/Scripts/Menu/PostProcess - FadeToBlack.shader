238c0bba888c4891f2efaded9c397edde7eb8c83
Martin <martin.lande@hotmail.com>
2022-10-03 00:58:19 +0200

    Hooked up Menu -> Game, added scene fade effect

---
+++ LDjam51/Assets/Scripts/Menu/PostProcess - FadeToBlack.shader
@@ -0,0 +1,81 @@
+﻿Shader "Custom/PostProcess-FadeToBlack"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Fade ("Fade", Range(0, 1)) = 0.5
+        _Smoothness ("Smoothness", Range(0, 1)) = 0.5
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        // We need to set these tags
+        Tags { "Queue"="Transparent" }
+
+        Pass
+        {
+        	// Blend mode
+        	Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            float _Fade;
+            float _Smoothness;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                // Get pixel coordinates (0 to 1 range)
+                float2 screenPos = i.uv.xy;
+
+                // TODO: Adjust circle to be uniform
+                // _ScreenParams: x is the width of the camera’s target texture in pixels, y is the height of the camera’s target texture in pixels, z is 1.0 + 1.0/width and w is 1.0 + 1.0/height.
+
+                float aspectRatio = _ScreenParams.x / _ScreenParams.y;
+                screenPos.x = screenPos.x * aspectRatio;
+
+                // Get distance from pixel to center of screen
+                float dis = distance(screenPos, float2(aspectRatio / 2, 0.5));
+
+                // Remap fade-variable
+                float newFade = (1.0f - _Fade) * (0.8f + _Smoothness) - _Smoothness;
+
+                //float endFade = smoothstep(0.0f, 1.0f, _Fade * _Fade);
+
+                col.a = smoothstep(newFade, newFade + _Smoothness, dis / (aspectRatio / 2 + 1 - _Smoothness));
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ LDjam51/Assets/Scripts/Menu/SceneSwitcher.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class SceneSwitcher : MonoBehaviour
+{
+	const float FADE_IN_TIME = 2f, FADE_OUT_TIME = 1f, WAIT_TIME = 0.3f;
+
+	static float fade = 1.0f;
+	static string queuedScene = "";
+	static GameObject fadeCanvas;
+	static Material fadeCanvasImageMaterial;
+
+    static public UnityEvent onUpdateFadeCanvas;
+
+    static public UnityEvent onFadeOut = new UnityEvent(), onEnterScene = new UnityEvent(), onExitScene = new UnityEvent();
+
+    static float initialWait;
+
+    private void Awake() {
+        if (onUpdateFadeCanvas == null) {
+            onUpdateFadeCanvas = new UnityEvent();
+        } else {
+            onEnterScene.Invoke();
+        }
+
+        initialWait = 0.1f;
+        UpdateFadeCanvas();
+    }
+
+    private void Start() {
+        onEnterScene.Invoke();
+    }
+
+    void Update()
+	{
+		if (queuedScene == "") {
+			if (fade > 0.0f && initialWait <= 0f) {
+				fade -= Time.unscaledDeltaTime / FADE_IN_TIME;
+
+				if (fade < 0.0f) {
+					fade = 0.0f;
+					fadeCanvas.SetActive(false);
+				}
+
+				UpdateFadeCanvas();
+			} else {
+                if (initialWait > 0f) {
+                    initialWait -= Time.deltaTime; // <- Don't use unscaled time for this one
+                }
+            }
+		} else {
+			if (fade < 1.0f) {
+				fade += Time.unscaledDeltaTime / FADE_OUT_TIME;
+
+				if (fade >= 1.0f) {
+					fade = 1.0f;
+					StartCoroutine(LoadQueuedScene());
+				}
+
+				UpdateFadeCanvas();
+			}
+		}
+	}
+
+    static public float GetFade() {
+        return fade;
+    }
+
+	static public void Restart()
+	{
+        GoTo(SceneManager.GetActiveScene().name);
+    }
+
+	static public void GoTo(string sceneName)
+	{
+		queuedScene = sceneName;
+		fadeCanvas.SetActive(true);
+        onFadeOut.Invoke();
+    }
+
+	static IEnumerator LoadQueuedScene()
+	{
+		yield return new WaitForSecondsRealtime(WAIT_TIME);
+
+        onExitScene.Invoke();
+
+        SceneManager.LoadScene(queuedScene);
+
+        queuedScene = "";
+	}
+
+	static void UpdateFadeCanvas()
+	{
+		// Create a fadecanvas if it doesn't exist
+		if (fadeCanvas == null) {
+			fadeCanvas = Instantiate(Resources.Load("FadeCanvas") as GameObject);
+
+			// Get material of fadecanvas image
+			fadeCanvasImageMaterial = fadeCanvas.transform.Find("Image").GetComponent<Image>().material;
+		}
+
+		// Make fadecanvas block raycasts only when fading out
+		fadeCanvas.transform.GetChild(0).GetComponent<Image>().raycastTarget = (queuedScene != "");
+
+		// Send fade-value to shader
+		fadeCanvasImageMaterial.SetFloat("_Fade", EasingFunction.EaseOutSine(0f, 1f, fade));
+
+        onUpdateFadeCanvas.Invoke();
+	}
+
+    public static string GetCurrentSceneName() {
+        return SceneManager.GetActiveScene().name;
+    }
+}
