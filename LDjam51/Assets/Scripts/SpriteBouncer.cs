5a6bb1855c8e0ba6d7bf74145614f6c25d36e4ab
Martin <martin.lande@hotmail.com>
2022-10-03 23:31:51 +0200

    Added bounce to the arrow for some reason???

---
+++ LDjam51/Assets/Scripts/SpriteBouncer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBouncer : MonoBehaviour
+{
+    public float bounceTime = 0.5f, waitTime = 1f;
+    public Vector2 movement;
+    public EasingFunction.Ease easeUp, easeDown;
+
+    SpriteRenderer rt;
+    Vector2 initialPosition;
+    float t, wait;
+    bool doBounce;
+    EasingFunction.Function easeUpFunc, easeDownFunc;
+
+    private void Awake() {
+        rt = GetComponent<SpriteRenderer>();
+        initialPosition = rt.transform.localPosition;
+        wait = waitTime;
+        t = 0.0f;
+        easeUpFunc = EasingFunction.GetEasingFunction(easeUp);
+        easeDownFunc = EasingFunction.GetEasingFunction(easeDown);
+    }
+
+    private void Update() {
+        if (doBounce) {
+            if (t < 1.0f) {
+                t += Time.unscaledDeltaTime / (bounceTime / 2f);
+
+                if (t >= 1.0f) {
+                    t = 1.0f;
+                    doBounce = false;
+                }
+            }
+        } else {
+            if (t > 0.0f) {
+                t -= Time.unscaledDeltaTime / (bounceTime / 2f);
+
+                if (t <= 0f) {
+                    t = 0.0f;
+                }
+            } else {
+                if (wait > 0.0f) {
+                    wait -= Time.unscaledDeltaTime / waitTime;
+                }
+                if (wait <= 0.0f) {
+                    doBounce = true;
+                    wait = waitTime;
+                }
+            }
+        }
+
+        float easeMove = easeUpFunc(0f, 1.0f, t);
+        if (!doBounce) {
+            easeMove = easeDownFunc(0f, 1.0f, t);
+        }
+        rt.transform.localPosition = new Vector2(initialPosition.x + (easeMove * movement.x), initialPosition.y + (easeMove * movement.y));
+    }
+
+    public void ResetPosition() {
+        if (rt != null) {
+            rt.transform.localPosition = initialPosition;
+        }
+    }
+}
