49c20c8bbf71d8644ef7dffd9bcf15ee91aba21d
Martin <martin.lande@hotmail.com>
2022-10-03 23:16:07 +0200

    Did some stuff

--- LDjam51/Assets/Scripts/Menu/LevelGoto.cs
+++ LDjam51/Assets/Scripts/Menu/LevelGoto.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class LevelGoto : MonoBehaviour
 {
+    [SerializeField]
+    EventReference eventOnGoTo;
+
     public void GoToLevel(int id) {
         Game.level = id;
         SceneSwitcher.GoTo("GameSingleScreen");
+        FMODUtility.Play(eventOnGoTo);
     }
 }
